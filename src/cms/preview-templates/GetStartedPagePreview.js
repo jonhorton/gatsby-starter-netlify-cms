6c7fb645c5ba13c2480f8bba88d80ec055f24688
Jon Horton <jonhorton@gmail.com>
2018-06-14 12:44:32 -0400

    adding new preview template files

---
+++ src/cms/preview-templates/GetStartedPagePreview.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { GetStartedPageTemplate } from '../../templates/get-started-page'
+
+const GetStartedPagePreview = ({ entry, widgetFor }) => (
+  <GetStartedPageTemplate
+    title={entry.getIn(['data', 'title'])}
+    content={widgetFor('body')}
+  />
+)
+
+GetStartedPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  widgetFor: PropTypes.func,
+}
+
+export default GetStartedPagePreview
