97c3c44c1985f2d111db091ee59532c0fc264a46
Jon Horton <jonhorton@gmail.com>
2018-06-14 12:27:54 -0400

    figuring this out as i go. have to edit cms preview-templates separately.

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,9 +2,15 @@ import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
+import SolutionsPagePreview from './preview-templates/SolutionsPagePreview'
+import PricingPagePreview from './preview-templates/PricingPagePreview'
+import ContactPagePreview from './preview-templates/ContactPagePreview'
+import GetStartedPagePreview from './preview-templates/AboutPagePreview'
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
+CMS.registerPreviewTemplate('solutions', SolutionsPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('pricing', PricingPagePreview)
+CMS.registerPreviewTemplate('contact', ContactPagePreview)
+CMS.registerPreviewTemplate('get-started', GetStartedPagePreview)
