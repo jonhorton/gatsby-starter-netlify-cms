44644b9de58a20a0090b6730599846b868c05fcd
Jon Horton <jonhorton@gmail.com>
2018-07-06 16:08:33 -0400

    finalized the blog list page

--- src/templates/blog-list-page.js
+++ src/templates/blog-list-page.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { kebabCase } from 'lodash'
 
 export default class BlogListPage extends React.Component {
   render() {
@@ -19,32 +20,49 @@ export default class BlogListPage extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-              <h1 className="is-size-2">Latest Stories</h1>
+              <h1 className="is-size-2">Latest Entries</h1>
             </div>
+            <div className="columns is-multiline">
             {posts
               .map(({ node: post }) => (
-                <div
-                  className="content"
-                  style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                  key={post.id}
-                >
-                  <p>
-                    <Link className="has-text-primary" to={post.fields.slug}>
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <small>{post.frontmatter.date}</small>
-                  </p>
-                  <p>
-                    {post.excerpt}
-                    <br />
-                    <br />
-                    <Link className="button is-small" to={post.fields.slug}>
-                      Keep Reading →
-                    </Link>
-                  </p>
-                </div>
+                  <div className="column is-one-third" key={post.id}>
+                    <div className="card">
+                      <div className="card-image">
+                        <figure className="image is-4by3">
+                          <Link className="has-text-primary" to={post.fields.slug}>
+                            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                          </Link>
+                        </figure>
+                      </div>
+                      <div className="card-content">
+                        <div className="content">
+                          <h3 className="title is-size-3">
+                            <Link className="has-text-primary" to={post.fields.slug}>
+                              {post.frontmatter.title}
+                            </Link>
+                          </h3>
+                          <p>
+                            <small>{post.frontmatter.date}</small>
+                            <span> | </span>
+                            {post.frontmatter.tags.map(tag => (
+                                <span><Link to={`/tags/${kebabCase(tag)}/`} key={tag + `tag`}>#{tag}</Link>&nbsp;</span>
+                            ))}
+                          </p>
+                          <p>
+                            {post.excerpt}
+                            <br />
+                            <br />
+                            <Link className="button is-small" to={post.fields.slug}>
+                              Read Article →
+                            </Link>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
               ))}
+            </div>
           </div>
         </section>
         <Footer/>
@@ -84,6 +102,7 @@ export const pageQuery = graphql`
             }
             templateKey
             date(formatString: "MMMM DD, YYYY")
+            tags
           }
         }
       }
